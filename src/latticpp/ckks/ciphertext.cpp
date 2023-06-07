782a61a0da6e80e3ce5e4f9ecb5fa0baa34a43cd
Guy Moshkowich <guy.moshkowich@ibm.com>
2023-06-07 18:26:32 +0300

    ciphertext.cpp format

--- src/latticpp/ckks/ciphertext.cpp
+++ src/latticpp/ckks/ciphertext.cpp
@@ -5,51 +5,48 @@
 
 namespace latticpp {
 
-uint64_t level(const Ciphertext &ct) {
-  return lattigo_level(ct.getRawHandle());
-}
+    uint64_t level(const Ciphertext &ct) {
+        return lattigo_level(ct.getRawHandle());
+    }
 
-double scale(const Ciphertext &ct) {
-  return lattigo_ciphertextScale(ct.getRawHandle());
-}
+    double scale(const Ciphertext &ct) {
+        return lattigo_ciphertextScale(ct.getRawHandle());
+    }
 
-uint64_t degree(const Ciphertext &ct) {
-  return lattigo_ciphertextDegree(ct.getRawHandle());
-}
+    uint64_t degree(const Ciphertext &ct) {
+      return lattigo_ciphertextDegree(ct.getRawHandle());
+    }
 
-Ciphertext copyNew(const Ciphertext &ct) {
-  // a handle of 0 is an invalid Go reference (my equivalent of a nil/null
-  // pointer)
-  if (ct.getRawHandle() == 0) {
-    return ct;
-  }
-  return lattigo_copyNew(ct.getRawHandle());
-}
+    Ciphertext copyNew(const Ciphertext &ct) {
+        // a handle of 0 is an invalid Go reference (my equivalent of a nil/null pointer)
+        if (ct.getRawHandle() == 0) {
+            return ct;
+        }
+        return lattigo_copyNew(ct.getRawHandle());
+    }
 
-Ciphertext newCiphertext(const Parameters &params, uint64_t degree,
-                         uint64_t level) {
-  return Ciphertext(
-      lattigo_newCiphertext(params.getRawHandle(), degree, level));
-}
+    Ciphertext newCiphertext(const Parameters &params, uint64_t degree, uint64_t level) {
+        return Ciphertext(lattigo_newCiphertext(params.getRawHandle(), degree, level));
+    }
 
-void set(const Ciphertext &ctx, MetaData &metaData) {
-  lattigo_setCiphertextMetaData(ctx.getRawHandle(), metaData.getRawHandle());
-}
+    void set(const Ciphertext &ctx, MetaData &metaData) {
+      lattigo_setCiphertextMetaData(ctx.getRawHandle(), metaData.getRawHandle());
+    }
 
-void set(const CiphertextQP &ctx, MetaData &metaData) {
-  lattigo_setCiphertextQPMetaData(ctx.getRawHandle(), metaData.getRawHandle());
-}
+    void set(const CiphertextQP &ctx, MetaData &metaData) {
+      lattigo_setCiphertextQPMetaData(ctx.getRawHandle(), metaData.getRawHandle());
+    }
 
-MetaData getMetaData(const CiphertextQP &ctxQP) {
-  return lattigo_getCiphertextQPMetaData(ctxQP.getRawHandle());
-}
+    MetaData getMetaData(const CiphertextQP &ctxQP) {
+      return lattigo_getCiphertextQPMetaData(ctxQP.getRawHandle());
+    }
 
-MetaData getMetaData(const Ciphertext &ctx) {
-  return lattigo_getCiphertextMetaData(ctx.getRawHandle());
-}
+    MetaData getMetaData(const Ciphertext &ctx) {
+      return lattigo_getCiphertextMetaData(ctx.getRawHandle());
+    }
 
-Poly poly(const Ciphertext &ctx, uint64_t i) {
-  return lattigo_poly(ctx.getRawHandle(), i);
-}
+    Poly poly(const Ciphertext &ctx, uint64_t i) {
+      return lattigo_poly(ctx.getRawHandle(), i);
+    }
 
-} // namespace latticpp
+}  // namespace latticpp
