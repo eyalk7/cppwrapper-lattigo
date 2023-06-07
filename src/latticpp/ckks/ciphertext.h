4dd4cb08e3a13aae49f7e70ce6e5c8dc4650e306
Guy Moshkowich <guy.moshkowich@ibm.com>
2023-06-07 18:30:16 +0300

    ciphertext.h format

--- src/latticpp/ckks/ciphertext.h
+++ src/latticpp/ckks/ciphertext.h
@@ -3,30 +3,28 @@
 
 #pragma once
 
-#include "cgo/ciphertext.h"
 #include "latticpp/marshal/gohandle.h"
+#include "cgo/ciphertext.h"
 
 namespace latticpp {
 
-uint64_t level(const Ciphertext &ct);
+    uint64_t level(const Ciphertext &ct);
 
-double scale(const Ciphertext &ct);
+    double scale(const Ciphertext &ct);
 
-uint64_t dergree(const Ciphertext &ct);
+    uint64_t degree(const Ciphertext &ct);
 
-Ciphertext copyNew(const Ciphertext &ct);
+    Ciphertext copyNew(const Ciphertext &ct);
 
-Ciphertext newCiphertext(const Parameters &params, uint64_t degree,
-                         uint64_t level);
+    Ciphertext newCiphertext(const Parameters &params, uint64_t degree, uint64_t level);
 
-void set(const Ciphertext &ctx, MetaData &metaData);
+    void set(const Ciphertext &ctx, MetaData &metaData);
 
-void set(const CiphertextQP &ctx, MetaData &metaData);
+    void set(const CiphertextQP &ctx, MetaData &metaData);
 
-MetaData getMetaData(const CiphertextQP &ctxQP);
+    MetaData getMetaData(const CiphertextQP &ctxQP);
 
-MetaData getMetaData(const Ciphertext &ctx);
+    MetaData getMetaData(const Ciphertext &ctx);
 
-Poly poly(const Ciphertext &ctx, uint64_t i);
-
-} // namespace latticpp
+    Poly poly(const Ciphertext &ctx, uint64_t i);
+}  // namespace latticpp
