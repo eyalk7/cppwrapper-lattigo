f6dd4199e03a73dd942db4eec297312095f46199
Guy Moshkowich <guy.moshkowich@ibm.com>
2023-06-08 11:24:45 +0300

    ring.h format

--- src/latticpp/ring/ring.h
+++ src/latticpp/ring/ring.h
@@ -8,50 +8,48 @@
 
 namespace latticpp {
 
-Ring newRing(uint64_t n, std::vector<uint64_t> moduli);
+    Ring newRing(uint64_t n, std::vector<uint64_t> moduli);
 
-Poly newPoly(const Ring &ring);
+    Poly newPoly(const Ring &ring);
 
-void add(const Ring &ring, const Poly &p1, const Poly &p2, Poly &p3);
+    void add(const Ring &ring, const Poly &p1, const Poly &p2, Poly &p3);
 
-void copy(Poly &pTarget, const Poly &pSrc);
+    void copy(Poly &pTarget, const Poly &pSrc);
 
-UniformSampler newUniformSampler(const PRNG &prng, const Ring &ring);
+    UniformSampler newUniformSampler(const PRNG &prng, const Ring &ring);
 
-Poly readNewFromSampler(const UniformSampler &sampler);
+    Poly readNewFromSampler(const UniformSampler &sampler);
 
-Poly polyQ(const PolyQP &polyqp);
+    Poly polyQ(const PolyQP &polyqp);
 
-Poly polyP(const PolyQP &polyqp);
+    Poly polyP(const PolyQP &polyqp);
 
-void copyLvl(uint64_t level, const Poly sourcePoly, Poly targetPoly);
+    void copyLvl(uint64_t level, const Poly sourcePoly, Poly targetPoly);
 
-void copyPolySingleLevel(const Poly sourcePoly, uint64_t sourceIndex,
-                         const Poly targetPoly, uint64_t targetIndex);
+    void copyPolySingleLevel(const Poly sourcePoly, uint64_t sourceIndex, const Poly targetPoly, uint64_t targetIndex);
 
-BasisExtender newBasisExtender(Ring ringQ, Ring ringP);
+    BasisExtender newBasisExtender(Ring ringQ, Ring ringP);
 
-void modUpQtoP(BasisExtender ext, int levelQ, int levelP, Poly polQ, Poly polP);
+    void modUpQtoP(BasisExtender ext, int levelQ, int levelP, Poly polQ, Poly polP);
 
-void invNTTLvl(RingQP ringqp, int levelQ, int levelP, PolyQP pIn, PolyQP pOut);
+    void invNTTLvl(RingQP ringqp, int levelQ, int levelP, PolyQP pIn, PolyQP pOut);
 
-void nTTLvl(RingQP ringqp, int levelQ, int levelP, PolyQP pIn, PolyQP pOut);
+    void nTTLvl(RingQP ringqp, int levelQ, int levelP, PolyQP pIn, PolyQP pOut);
 
-void invMFormLvl(RingQP ringqp, int levelQ, int levelP, PolyQP pIn,
-                 PolyQP pOut);
+    void invMFormLvl(RingQP ringqp, int levelQ, int levelP, PolyQP pIn, PolyQP pOut);
 
-void mFormLvl(RingQP ringqp, int levelQ, int levelP, PolyQP pIn, PolyQP pOut);
+    void mFormLvl(RingQP ringqp, int levelQ, int levelP, PolyQP pIn, PolyQP pOut);
 
-void invMFormLvl(Ring ring, int level, Poly pIn, Poly pOut);
+    void invMFormLvl(Ring ring, int level, Poly pIn, Poly pOut);
 
-void mFormLvl(Ring ring, int level, Poly pIn, Poly pOut);
+    void mFormLvl(Ring ring, int level, Poly pIn, Poly pOut);
 
-uint64_t degree(Poly p);
+    uint64_t degree(Poly p);
 
-uint64_t N(Ring ring);
+    uint64_t N(Ring ring);
 
-std::vector<uint64_t> permuteNTTIndex(Ring ring, uint64_t galEl);
+    std::vector<uint64_t> permuteNTTIndex(Ring ring, uint64_t galEl);
 
-void permuteNTTWithIndexLvl(Ring ring, uint64_t level, Poly polyIn,
-                            const std::vector<uint64_t> &index, Poly polyOut);
+    void permuteNTTWithIndexLvl(Ring ring, uint64_t level, Poly polyIn, const std::vector<uint64_t> &index, Poly polyOut);
+
 } // namespace latticpp
